9672f52c6c61c613db6c8e255e550f4b8d899541
Akhilreddymalladi <105489408+Akhil-132@users.noreply.github.com>
2023-08-29 18:27:32 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -2,7 +2,8 @@
 sudo apt-get update
 sudo apt-get install -y nginx wget
 sudo systemctl start nginx
-wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
-unzip -o 2117_infinite_loop.zip
-cp -r 2117_infinite_loop/* /var/www/html/
+wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
+unzip -o 2137_barista_cafe.zip
+rm -rf /var/www/html/ *
+cp -r 2137_barista_cafe/* /var/www/html/
 systemctl restart nginx
